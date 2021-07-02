551a82b150c7828442f1dcd753438948d8b65978
owentangjintanakarn <owen.t95@hotmail.com>
2021-07-02 18:18:56 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet'); //HTTP Headers
+const morgan = require('morgan'); //Logging
+//App Setup
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+app.use(helmet());
+app.use(morgan('tiny'))
+app.set('json spaces', 2);
+//Middlewares
+
+//Routes
+app.get('/', (req, res) => {
+    res.json({message: 'ROOT of IMS BACKEND EXPRESS'})
+});
+
+
+
+//Listen
+app.listen(PORT, () => {
+    console.log(`Live on port ${PORT}`)
+});
