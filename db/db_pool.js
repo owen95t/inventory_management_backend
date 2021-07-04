4a0089318da7adcb410954cebd6bfec52f4d807c
owentangjintanakarn <owen.t95@hotmail.com>
2021-07-04 21:38:06 +0700

    Added query method exported from dbpool

--- db/db_pool.js
+++ db/db_pool.js
@@ -3,4 +3,8 @@ const config = require('../db/db_config')
 
 const pool = new Pool(config)
 
-module.exports = pool
+module.exports = {
+    query: (text, params, callback) => {
+        return pool.query(text, params, callback)
+    },
+}
